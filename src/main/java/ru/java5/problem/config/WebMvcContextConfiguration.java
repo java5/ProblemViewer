687cd0c39a84e312bd1c9598b00eff841ebee4a5
java5 <radik.zainullin@gmail.com>
2013-01-17 22:02:08 +0400

    19 перед добавлением страницы где будут показаны задачи

--- src/main/java/ru/java5/problem/config/WebMvcContextConfiguration.java
+++ src/main/java/ru/java5/problem/config/WebMvcContextConfiguration.java
@@ -1,6 +1,9 @@
 package ru.java5.problem.config;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import java.util.Properties;
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +12,11 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -18,57 +26,162 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-/**
- * Configures Spring MVC.
- *
- * @author 
- */
 @Configuration
+@EnableJpaRepositories("ru.java5.problem.jpa.repository")
+@EnableTransactionManagement
 @EnableWebMvc
-@ComponentScan(basePackages = {"ru.java5.problem"})
+@ComponentScan(basePackages = {
+  "ru.java5.problem.jpa.controller",
+  "ru.java5.problem.jpa.service"
+})
 @PropertySource("classpath:application.properties")
 @ImportResource("WEB-INF/problem-security.xml")
 public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+  private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+  private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+  private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+  private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+  private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+  private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
+  private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+  private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
+  private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+  private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
   @Resource
-  private Environment environment;  
+  private Environment environment;
 
   @Override
   public void addResourceHandlers(final ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/resources/**/*").addResourceLocations("classpath:/META-INF/web-resources/");
   }
+
+  /**
+   * Ensures that dispatcher servlet can be mapped to '/' and static resources are still served by the containers
+   * default servlet.
+   *
+   * @param configurer
+   */
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
   @Override
   public void addViewControllers(final ViewControllerRegistry registry) {
     registry.addViewController("/index.htm").setViewName("public/index");
     registry.addViewController("/problems.htm").setViewName("public/problems");
   }
-  @Override
-  public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
-    configurer.enable();
+
+  /**
+   * Configures the data source.
+   *
+   * @return
+   */
+  @Bean
+  public DataSource dataSource() {
+    BoneCPDataSource dataSource = new BoneCPDataSource();
+
+    dataSource.setDriverClass(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+    dataSource.setJdbcUrl(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+    dataSource.setUsername(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+    dataSource.setPassword(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+
+    return dataSource;
   }
+
   @Bean
   public HandlerInterceptor localeChangeInterceptor() {
     LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
     localeChangeInterceptor.setParamName("lang");
     return localeChangeInterceptor;
   }
+
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    InterceptorRegistration registration;
-    registration = registry.addInterceptor(localeChangeInterceptor());
+    InterceptorRegistration registration = registry.addInterceptor(localeChangeInterceptor());
     registration.addPathPatterns("/**");
   }
+
   @Bean
   public LocaleResolver localeResolver() {
     return new CookieLocaleResolver();
   }
+
   @Bean
   public MessageSource messageSource() {
     ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-    messageSource.setBasenames(new String[]{ "WEB-INF/i18n/messages", "WEB-INF/i18n/application" });
+    messageSource.setBasenames(new String[]{"WEB-INF/i18n/messages", "WEB-INF/i18n/application"});
     messageSource.setUseCodeAsDefaultMessage(true);
     return messageSource;
   }
+
+  /**
+   * Configures the transaction manager.
+   *
+   * @return
+   */
+  @Bean
+  public JpaTransactionManager transactionManager() {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+    return transactionManager;
+  }
+
+  /**
+   * Configures the entity manager factory.
+   *
+   * @return
+   */
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+    entityManagerFactoryBean.setDataSource(dataSource());
+    entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+    entityManagerFactoryBean.setPackagesToScan(environment.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+
+    Properties jpaProperties = new Properties();
+    jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+    jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
+    jpaProperties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
+    jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
+    jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+
+    entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+    return entityManagerFactoryBean;
+  }
+
+  /**
+   * Configures the exception resolver.
+   *
+   * @return
+   */
+//  @Bean
+//  public SimpleMappingExceptionResolver exceptionResolver() {
+//    SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
+//
+//    Properties exceptionMappings = new Properties();
+//
+//    exceptionMappings.put("com.packtpub.springdata.jpa.service.NotFoundException", "error/404");
+//    exceptionMappings.put("java.lang.Exception", "error/error");
+//    exceptionMappings.put("java.lang.RuntimeException", "error/error");
+//
+//    exceptionResolver.setExceptionMappings(exceptionMappings);
+//
+//    Properties statusCodes = new Properties();
+//
+//    statusCodes.put("error/404", "404");
+//    statusCodes.put("error/error", "500");
+//
+//    exceptionResolver.setStatusCodes(statusCodes);
+//
+//    return exceptionResolver;
+//  }
 }
---
+++ src/main/java/ru/java5/problem/jpa/service/NotFoundException.java
@@ -0,0 +1,11 @@
+package ru.java5.problem.jpa.service;
+
+/**
+ * This exception is thrown if the requested contact is not found.
+ * @author Petri Kainulainen
+ */
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
